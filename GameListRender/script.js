c5513df43eba2382748f5e0d20163ebbbb8b43f7
Damian E. Pereyra <85878761+Damian-Pereyra-dv@users.noreply.github.com>
2023-09-25 19:11:11 -0300

    Add files via upload

---
+++ GameListRender/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const detalleVideojuego = document.getElementById("detalle-videojuego");
+  
+    // Obtener el ID del videojuego desde la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const idVideojuego = urlParams.get("id");
+  
+    // Obtener los datos de los videojuegos desde el localStorage
+    const datosVideojuegos = JSON.parse(localStorage.getItem("videojuegos"));
+  
+    // Buscar el videojuego por ID
+    const videojuegoSeleccionado = datosVideojuegos.videojuegos.find(
+      (videojuego) => videojuego.id == idVideojuego
+    );
+  
+    if (videojuegoSeleccionado) {
+      // Crear un elemento div para mostrar los detalles del videojuego y agregarlo al DOM
+      const contenedorDetallesVideojuego = document.createElement("div");
+      contenedorDetallesVideojuego.classList.add("detalles-videojuego");
+      contenedorDetallesVideojuego.innerHTML = `
+      <h2>${videojuegoSeleccionado.nombre}</h2>
+      <p>ID: ${videojuegoSeleccionado.id}</p>
+      <p>Género: ${videojuegoSeleccionado.genero}</p>
+      <p>Plataforma: ${videojuegoSeleccionado.plataforma}</p>
+      <img src="${videojuegoSeleccionado.imagen}" alt="${videojuegoSeleccionado.nombre}" />
+      `;
+      // Agregar al página principal
+      detalleVideojuego.appendChild(contenedorDetallesVideojuego);
+    }
+  });
---
+++ GameListRender/setGames.js
@@ -0,0 +1,30 @@
+fetch("./videojuegos.json")
+  .then((respuesta) => respuesta.json())
+  .then((datos) => localStorage.setItem("videojuegos", JSON.stringify(datos)));
+
+document.addEventListener("DOMContentLoaded", () => {
+    const gridVideojuegos = document.getElementById("grid-videojuegos");
+    const datosVideojuegos = JSON.parse(localStorage.getItem("videojuegos"));
+    console.log(datosVideojuegos);
+    if (datosVideojuegos) {
+      datosVideojuegos.videojuegos.forEach((videojuego) => {
+        //   1.- Creamos un elemento <div> para cada videojuego
+        const gridItem = document.createElement("div");
+        //   2.- Agregamos la clase "grid-item" al div que contiene el nombre del videojuego y su información
+        gridItem.classList.add("grid-item");
+        //    3.- Agregamos una etiqueta h4 con el texto del título a nuestro nuevo elemento <div>.
+        gridItem.innerHTML = `<h4>${videojuego.nombre}</h4>`;
+  
+        gridItem.addEventListener("click", () => {
+          mostrarDetallesVideojuego(videojuego);
+        });
+  
+        gridVideojuegos.appendChild(gridItem);
+      });
+    }
+  });
+  
+  function mostrarDetallesVideojuego(videojuego) {
+    // Redireccionar a otra página para mostrar los detalles
+    window.location.href = `videojuego.html?id=${videojuego.id}`;
+  }
